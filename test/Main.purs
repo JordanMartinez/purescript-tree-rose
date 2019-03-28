4c37646276bc6b025aca488b4be8f8c1823f7ce9
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-03-27 17:50:19 -0700

    Add test that fails when using `lastChild` and `insertAfter`

--- test/Main.purs
+++ test/Main.purs
@@ -6,7 +6,7 @@ import Control.Comonad.Cofree (head, (:<))
 import Data.List (List(Nil), (:))
 import Data.Maybe (Maybe(..), fromJust)
 import Data.Tree (Tree, mkTree, scanTree)
-import Data.Tree.Zipper (down, findDownWhere, findFromRoot, findUp, flattenLocDepthFirst, fromTree, insertAfter, modifyValue, next, toTree, value)
+import Data.Tree.Zipper (down, findDownWhere, findFromRoot, findUp, flattenLocDepthFirst, fromTree, insertAfter, modifyValue, next, toTree, value, firstChild, lastChild)
 import Effect (Effect)
 import Partial.Unsafe (unsafePartial)
 import Test.Spec (describe, it)
@@ -15,13 +15,13 @@ import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (run)
 
 sampleTree :: Tree Int
-sampleTree = 
-  1 :< 
+sampleTree =
+  1 :<
       (2 :< Nil)
     : (3 :< Nil)
     : (4 :<
           (5 :< Nil)
-        : (6 :< 
+        : (6 :<
             (7 :< Nil) : Nil)
         : (8 :< Nil)
         : Nil
@@ -29,21 +29,21 @@ sampleTree =
     : Nil
 
 main :: Effect Unit
-main = run [consoleReporter] do 
-  describe "Tree" do    
+main = run [consoleReporter] do
+  describe "Tree" do
 
     it "mkTree" do
       let t = mkTree 10 Nil
       shouldEqual (head t) 10
 
     it "Functor" do
-      let result = 
-              2 :< 
+      let result =
+              2 :<
                   (3 :< Nil)
                 : (4 :< Nil)
                 : (5 :<
                       (6 :< Nil)
-                    : (7 :< 
+                    : (7 :<
                         (8 :< Nil) : Nil)
                     : (9 :< Nil)
                     : Nil
@@ -52,35 +52,35 @@ main = run [consoleReporter] do
       shouldEqual (eq (((+)1) <$> sampleTree) result) true
 
     it "scanTree" do
-      let result = 
-              1 :< 
+      let result =
+              1 :<
                   (3 :< Nil)
                 : (4 :< Nil)
                 : (5 :<
                       (10 :< Nil)
-                    : (11 :< 
+                    : (11 :<
                         (18 :< Nil) : Nil)
                     : (13 :< Nil)
                     : Nil
                   )
                 : Nil
       shouldEqual (eq (scanTree (\a b -> a + b) 0 sampleTree) result) true
-  
+
   describe "Zipper" do
 
     let root = fromTree sampleTree
 
     it "Modify" do
 
-      let root' = unsafePartial $ toTree $ modifyValue (\a -> 2 * a) (fromJust $ down root)      
-      let root'' = unsafePartial $ toTree $ modifyValue (\a -> 2 * a) (fromJust $ down >=> next >=> next >=> down $ root) 
+      let root' = unsafePartial $ toTree $ modifyValue (\a -> 2 * a) (fromJust $ down root)
+      let root'' = unsafePartial $ toTree $ modifyValue (\a -> 2 * a) (fromJust $ down >=> next >=> next >=> down $ root)
       let result =
-              1 :< 
+              1 :<
                   (4 :< Nil)
                 : (3 :< Nil)
                 : (4 :<
                       (5 :< Nil)
-                    : (6 :< 
+                    : (6 :<
                         (7 :< Nil) : Nil)
                     : (8 :< Nil)
                     : Nil
@@ -88,12 +88,12 @@ main = run [consoleReporter] do
                 : Nil
 
       let result' =
-              1 :< 
+              1 :<
                   (2 :< Nil)
                 : (3 :< Nil)
                 : (4 :<
                       (10 :< Nil)
-                    : (6 :< 
+                    : (6 :<
                         (7 :< Nil) : Nil)
                     : (8 :< Nil)
                     : Nil
@@ -102,42 +102,74 @@ main = run [consoleReporter] do
 
       shouldEqual (eq root' result) true
       shouldEqual (eq root'' result') true
-    
+
     it "Insert" do
 
-      let root' = unsafePartial $ toTree $ insertAfter (mkTree 100 Nil) (fromJust $ down root)
-      let root'' = unsafePartial $ toTree $ insertAfter (mkTree 100 Nil) (fromJust $ (down root) >>= next >>= next >>= down >>= next >>= down)             
+      let root1 = unsafePartial $ toTree $ insertAfter (mkTree 100 Nil) (fromJust $ down root)
+      let root2 = unsafePartial $ toTree $ insertAfter (mkTree 100 Nil) (fromJust $ (down root) >>= next >>= next >>= down >>= next >>= down)
+      let root3 = unsafePartial $ toTree $ insertAfter (mkTree 100 Nil) (fromJust $ (firstChild root))
+      let root4 = unsafePartial $ toTree $ insertAfter (mkTree 100 Nil) (fromJust $ (lastChild root))
 
-      let result =
-              1 :< 
+      let result1 =
+              1 :<
                   (2 :< Nil)
                 : (100 :< Nil)
                 : (3 :< Nil)
                 : (4 :<
                       (5 :< Nil)
-                    : (6 :< 
+                    : (6 :<
                         (7 :< Nil) : Nil)
                     : (8 :< Nil)
                     : Nil
                   )
                 : Nil
 
-      let result' =
-              1 :< 
+      let result2 =
+              1 :<
                   (2 :< Nil)
                 : (3 :< Nil)
                 : (4 :<
                       (5 :< Nil)
-                    : (6 :< 
-                          (7 :< Nil) 
+                    : (6 :<
+                          (7 :< Nil)
                         : (100 :< Nil)
                         : Nil)
                     : (8 :< Nil)
                     : Nil
                   )
                 : Nil
-      shouldEqual (eq root' result) true
-      shouldEqual (eq root'' result') true
+      let result3 =
+              1 :<
+                  (2 :< Nil)
+                : (100 :< Nil)
+                : (3 :< Nil)
+                : (4 :<
+                      (5 :< Nil)
+                    : (6 :<
+                             (7 :< Nil)
+                           : Nil
+                      )
+                    : (8 :< Nil)
+                    : Nil
+                  )
+                : Nil
+      let result4 =
+              1 :<
+                  (2 :< Nil)
+                : (3 :< Nil)
+                : (4 :<
+                      (5 :< Nil)
+                    : (6 :<
+                        (7 :< Nil) : Nil)
+                    : (8 :< Nil)
+                    : Nil
+                  )
+                : (100 :< Nil)
+                : Nil
+      shouldEqual (eq root1 result1) true
+      shouldEqual (eq root2 result2) true
+      shouldEqual (eq root3 result3) true
+      shouldEqual (eq root4 result4) true
 
     it "Should findDownWhere with single node" do
       let tree = 1 :< Nil
@@ -145,96 +177,96 @@ main = run [consoleReporter] do
       shouldEqual (Just 1) ((findDownWhere (_ == 1) loc) <#> value)
 
     it "Should findDownWhere with 2 nodes and 2 levels" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
+      let tree = 1 :<
+                      (2 :< Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
       shouldEqual (Just 2) ((findDownWhere (_ == 2) loc) <#> value)
 
     it "Should findDownWhere with 3 nodes and 2 levels" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
-                    : (3 :< Nil) 
+      let tree = 1 :<
+                      (2 :< Nil)
+                    : (3 :< Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
       shouldEqual (Just 3) ((findDownWhere (_ == 3) loc) <#> value)
 
     it "Should findDownWhere with 4 nodes and 2 levels" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
-                    : (3 :< Nil) 
-                    : (4 :< Nil) 
+      let tree = 1 :<
+                      (2 :< Nil)
+                    : (3 :< Nil)
+                    : (4 :< Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
       shouldEqual (Just 4) ((findDownWhere (_ == 4) loc) <#> value)
 
     it "Should findDownWhere with 5 nodes and 3 levels" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
-                    : (3 :< Nil) 
-                    : (4 :< 
+      let tree = 1 :<
+                      (2 :< Nil)
+                    : (3 :< Nil)
+                    : (4 :<
                           (5 :< Nil)
-                        : Nil) 
+                        : Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
       shouldEqual (Just 5) ((findDownWhere (_ == 5) loc) <#> value)
 
     it "Should findDownWhere with 6 nodes and 3 levels" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
-                    : (3 :< Nil) 
-                    : (4 :< 
+      let tree = 1 :<
+                      (2 :< Nil)
+                    : (3 :< Nil)
+                    : (4 :<
                           (5 :< Nil)
                         : (6 :< Nil)
-                        : Nil) 
+                        : Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
       shouldEqual (Just 6) ((findDownWhere (_ == 6) loc) <#> value)
 
     it "Should findDownWhere with 7 nodes and 4 levels" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
-                    : (3 :< Nil) 
-                    : (4 :< 
+      let tree = 1 :<
+                      (2 :< Nil)
+                    : (3 :< Nil)
+                    : (4 :<
                           (5 :< Nil)
-                        : (6 :< 
+                        : (6 :<
                               (7 :< Nil) : Nil)
-                        : Nil) 
+                        : Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
       shouldEqual (Just 7) ((findDownWhere (_ == 7) loc) <#> value)
 
     it "Should findDownWhere with 8 nodes and 4 levels" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
-                    : (3 :< Nil) 
-                    : (4 :< 
+      let tree = 1 :<
+                      (2 :< Nil)
+                    : (3 :< Nil)
+                    : (4 :<
                           (5 :< Nil)
-                        : (6 :< 
+                        : (6 :<
                               (7 :< Nil) : Nil)
                         : (8 :< Nil)
-                        : Nil) 
+                        : Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
       shouldEqual (Just 8) ((findDownWhere (_ == 8) loc) <#> value)
 
     it "Should findDownWhere with 8 nodes and 4 levels with a step back" do
-      let tree = 1 :< 
-                      (2 :< Nil) 
-                    : (3 :< Nil) 
-                    : (4 :< 
+      let tree = 1 :<
+                      (2 :< Nil)
+                    : (3 :< Nil)
+                    : (4 :<
                           (5 :< Nil)
-                        : (6 :< 
+                        : (6 :<
                               (7 :< Nil) : Nil)
                         : (8 :< Nil)
-                        : Nil) 
+                        : Nil)
                       : Nil
       -- log $ showTree tree
       let loc = fromTree tree
@@ -260,6 +292,3 @@ main = run [consoleReporter] do
       --log $ showTree sampleTree
       --log $ show flat
       shouldEqual flat (1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : Nil)
-
-
-
